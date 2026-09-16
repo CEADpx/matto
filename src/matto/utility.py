746550046889dfcabe45657710fa0a8aee1a1e98
Prashant K. Jha <pjha.sci@gmail.com>
2026-09-15 22:32:42 -0600

    Drop unused FEniTop helpers from utility.py
    plot_design, create_mechanism_vectors, LinearProblem, Plotter and
    save_xdmf were carried over from FEniTop and are not used: nothing in
    src, examples or tests references them.
    
    compare_matrices stays; Communicator uses it for the serial/parallel
    dof map. Trimmed the imports that went with the removed code, which
    also drops the pyvista import from the package entirely.

--- src/matto/utility.py
+++ src/matto/utility.py
@@ -23,12 +23,8 @@ from dolfinx.nls.petsc import NewtonSolver
 import numpy as np
 from scipy.spatial import cKDTree
 from petsc4py import PETSc
-import dolfinx.io
-from dolfinx.fem import form, Function
-from dolfinx import la
-from dolfinx.fem.petsc import (create_vector, create_matrix,
-                               assemble_vector, assemble_matrix, set_bc)
-import pyvista
+import dolfinx.fem
+from dolfinx.fem import Function
 
 
 DEFAULT_STATE_SOLVER = {
@@ -197,109 +193,6 @@ class WrapNonlinearProblem:
 
     def __del__(self):
         self.solver.krylov_solver.destroy()
-        
-def plot_design(mesh, density, tag = None, path="", threshold=0.49, smooth_iter=100, pv_or_image = "pv"):
-    """Initialize a plotter."""
-    pyvista.OFF_SCREEN = True
-    # pyvista.start_xvfb()
-    dim = mesh.topology.dim
-    elements, cell_types, nodes = dolfinx.plot.vtk_mesh(mesh, dim)
-    grid = pyvista.UnstructuredGrid(elements, cell_types, nodes)
-
-    grid.point_data["density"] = np.hstack(density)
-    
-    plotter = pyvista.Plotter()
-    # plotter.disable()
-    plotter.background_color = "white"
-    lighting = dim == 3
-    plotter.add_mesh(grid, clim=[0, 1], cmap="Greys", lighting=lighting,
-                        show_scalar_bar=False)
-    
-    if dim == 2:
-        plotter.view_xy()
-    
-    filename = path+"optimized_design"
-    if tag is not None:
-        filename = filename + "_" + tag
-    plotter.screenshot(filename + ".png", window_size=(1000, 1000))    
-        
-def create_mechanism_vectors(func_space, in_spring, out_spring):
-    """Create vectors for compliant mechanism design."""
-    index_map = func_space.dofmap.index_map
-    block_size = func_space.dofmap.index_map_bs
-    spring_vec = la.create_petsc_vector(index_map, block_size)
-    l_vec = spring_vec.copy()
-    
-    local_range = index_map.local_range
-    local_indices = np.arange(local_range[0], local_range[1]).astype(np.int32)
-    local_size = np.ptp(local_range)
-    local_nodes = func_space.tabulate_dof_coordinates()[:local_size]
-    
-    for n, (locator, direction, value) in enumerate([in_spring, out_spring]):
-        ctrl_nodes = local_indices[locator(local_nodes.T)]
-        offset = ["x", "y", "z"].index(direction)
-        ctrl_dofs = ctrl_nodes*block_size + offset
-        spring_vec.setValues(ctrl_dofs, [value,]*ctrl_dofs.size)
-        if n == 1:
-            l_vec.setValues(ctrl_dofs, [1.0,]*ctrl_dofs.size)
-    
-    spring_vec.assemble()
-    l_vec.assemble()
-    return spring_vec, l_vec
-
-class LinearProblem:
-    def __init__(self, u, lam, lhs, rhs, l_vec, spring_vec, bcs=[], petsc_options={}):
-        """Initialize a linear problem."""
-        # Initialization
-        self.u, self.lam = u, lam
-        self.u_wrap = la.create_petsc_vector_wrap(self.u.x)
-        self.lam_wrap = la.create_petsc_vector_wrap(self.lam.x)
-        self.lhs_form, self.rhs_form = form(lhs), form(rhs)
-        self.lhs_mat = create_matrix(self.lhs_form)
-        self.rhs_vec = create_vector(self.rhs_form)
-        self.bcs, self.l_vec, self.spring_vec = bcs, l_vec, spring_vec
-
-        # Construct a linear solver
-        self.solver = PETSc.KSP().create(self.u.function_space.mesh.comm)
-        self.solver.setOperators(self.lhs_mat)
-        prefix = apply_petsc_options(
-            self.solver,
-            petsc_options,
-            prefix=f"linear_solver_{id(self)}",
-        )
-        for var in [self.lhs_mat, self.rhs_vec, self.l_vec]:
-            if var is not None:
-                var.setOptionsPrefix(prefix)
-                var.setFromOptions()
-
-        assemble_vector(self.rhs_vec, self.rhs_form)
-        self.rhs_vec.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-        set_bc(self.rhs_vec, self.bcs)
-
-    def solve_fem(self):
-        """Solve K*x=F for FEM."""
-        self.lhs_mat.zeroEntries()
-        assemble_matrix(self.lhs_mat, self.lhs_form, bcs=self.bcs)
-        self.lhs_mat.assemble()
-        if self.spring_vec is not None:
-            self.lhs_mat.setDiagonal(self.lhs_mat.getDiagonal()+self.spring_vec)
-        self.solver.solve(self.rhs_vec, self.u_wrap)
-        self.u.x.scatter_forward()
-
-    def solve_adjoint(self):
-        """Solve K*lambda=-L for the adjoint equation."""
-        self.solver.solve(-self.l_vec, self.lam_wrap)
-        self.lam.x.scatter_forward()
-        
-    def __del__(self):
-        self.solver.destroy()
-        self.lhs_mat.destroy()
-        self.rhs_vec.destroy()
-        self.u_wrap.destroy()
-        self.lam_wrap.destroy()
-        if self.spring_vec is not None:
-            self.spring_vec.destroy()
-            self.l_vec.destroy()    
 
 
 class Communicator():
@@ -382,42 +275,3 @@ def compare_matrices(array1, array2, precision=12, k=1):
     """Find the "args" such that array1[args] == array2."""
     kd_tree = cKDTree(array1.round(precision))
     return kd_tree.query(array2.round(precision), k=k)[1]
-
-
-class Plotter():
-    def __init__(self, mesh):
-        """Initialize a plotter."""
-        pyvista.OFF_SCREEN = True
-        # pyvista.start_xvfb()
-        self.dim = mesh.topology.dim
-        elements, cell_types, nodes = dolfinx.plot.vtk_mesh(mesh, self.dim)
-        self.grid = pyvista.UnstructuredGrid(elements, cell_types, nodes)
-
-    def plot(self, density, threshold=0.49, smooth_iter=100, path=""):
-        self.grid.point_data["density"] = np.hstack(density)
-        if self.dim == 2:
-            grid = self.grid
-        else:
-            grid = self.grid.threshold(threshold).extract_surface()
-        empty_mesh = (self.dim == 3 and grid.n_faces_strict == 0)
-        
-        if not empty_mesh:
-            if self.dim == 3:
-                grid = grid.smooth(n_iter=smooth_iter)
-                grid.point_data["density"] = 0.4
-            plotter = pyvista.Plotter()
-            plotter.background_color = "white"
-            lighting = self.dim == 3
-            plotter.add_mesh(grid, clim=[0, 1], cmap="Greys", lighting=lighting,
-                             show_scalar_bar=False)
-            if self.dim == 2:
-                plotter.view_xy()
-            plotter.screenshot(path+"optimized_design.jpg", window_size=(1000, 1000))
-            plotter.close()
-
-
-def save_xdmf(mesh, rho, path=""):
-    xdmf = dolfinx.io.XDMFFile(mesh.comm, path+"optimized_design.xdmf", "w")
-    xdmf.write_mesh(mesh)
-    rho.name = "density"
-    xdmf.write_function(rho)
